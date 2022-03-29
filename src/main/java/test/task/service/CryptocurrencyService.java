f00d51853b55bd9af203fad463356e1db80cf22d
Myron <shikhovmyron@gmail.com>
2022-03-29 19:33:45 +0200

    add methods for save crypto info

--- src/main/java/test/task/service/CryptocurrencyService.java
+++ src/main/java/test/task/service/CryptocurrencyService.java
@@ -1,16 +1,50 @@
 package test.task.service;
 
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
+import test.task.config.AppConfiguration;
+import test.task.model.CryptocurrencyInfoModel;
 import test.task.repository.CryptocurrencyInfoRepository;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static test.task.utils.JsonInfoModel.getCryptoInfoFromJsonCEX;
+import static test.task.utils.JsonInfoModel.getJsonFromUrl;
 
 @Service
 public class CryptocurrencyService {
 
+    private final AppConfiguration appConfiguration;
     private final CryptocurrencyInfoRepository cryptoInfoRepo;
 
-    public CryptocurrencyService(CryptocurrencyInfoRepository cryptoInfoRepo) {
+    public CryptocurrencyService(
+            AppConfiguration appConfiguration, CryptocurrencyInfoRepository cryptoInfoRepo
+    ) {
+        this.appConfiguration = appConfiguration;
         this.cryptoInfoRepo = cryptoInfoRepo;
     }
 
 
+    public List<CryptocurrencyInfoModel> getCryptocurrencyInfo() throws IOException {
+        List<CryptocurrencyInfoModel> cryptoInfoList = new ArrayList<>();
+
+        for (String currencyPair : appConfiguration.currencyPairList()) {
+            String url = String.format("https://cex.io/api/last_price/%s", currencyPair);
+            JSONObject json = getJsonFromUrl(url);
+            cryptoInfoList.add(getCryptoInfoFromJsonCEX(json));
+        }
+
+        return cryptoInfoList;
+    }
+
+    public void saveCryptocurrencyInfo() {
+        try {
+            List<CryptocurrencyInfoModel> cryptocurrencyInfo = getCryptocurrencyInfo();
+            cryptoInfoRepo.saveAll(cryptocurrencyInfo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
